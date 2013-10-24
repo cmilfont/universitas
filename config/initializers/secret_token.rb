d7c724a71ac67faf7c61a54fba9eb89859e9636e
Christiano Milfont <cmilfont@gmail.com>
2013-10-24 15:31:01 -0300

    estrutura inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Universitas::Application.config.secret_key_base = 'e19fa02c168df75cf2b443757e820671d63ed03438af0f6535f13a80b5438fb02101953bd0906ee4b75b1a41610db15b38d1740a1c9c2ba89bf599ef50765131'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Universitas::Application.config.session_store :cookie_store, key: '_universitas_session'
