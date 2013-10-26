5d9daa72f564852a0a969ef46367abb44c83b3a7
Fabio Lima <fabionl@gmail.com>
2013-10-26 17:23:28 -0300

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20131027112407) do
     t.datetime "updated_at"
   end
 
+  create_table "inscricoes", force: true do |t|
+    t.boolean  "pago"
+    t.integer  "turma_id"
+    t.integer  "aluno_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "instrutors", force: true do |t|
     t.string   "nome"
     t.datetime "created_at"
