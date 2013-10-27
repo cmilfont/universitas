8a3d3ac6fedcdb957ee6e1ab330d3743527da473
Christiano Milfont <cmilfont@gmail.com>
2013-10-27 10:20:20 -0300

    AJustando identação e evitando de criar um user

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -5,25 +5,25 @@ describe Users::OmniauthCallbacksController do
   describe 'Logar via Facebook' do
 
     let(:current_user) {
-      FactoryGirl.create :user
+      FactoryGirl.build :user
     }
 
     before do
 
-      controller.stub :current_user => current_user
+      #controller.stub :current_user => current_user
 
       OmniAuth.config.test_mode =  true
       OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-                                                                      :provider => 'facebook',
-                                                                      :uid => '123545',
-                                                                      'extra' => {
-                                                                        'raw_info' => {
-                                                                          'name' => "Teste Nome",
-                                                                          'email' => "joesmith@gmail.coa"
-                                                                        }
-                                                                      },
-                                                                      'credentials' => {'token' => "meu_cratim"}
-                                                                    })
+        :provider => 'facebook',
+        :uid => '123545',
+        'extra' => {
+          'raw_info' => {
+            'name' => "Teste Nome",
+            'email' => "joesmith@gmail.coa"
+          }
+        },
+        'credentials' => {'token' => "meu_cratim"}
+      })
 
       request.env["devise.mapping"] = Devise.mappings[:user]
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
